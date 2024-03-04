57d975341a72d6154d6849ff781f0077c5bf9dae
kartikey <98679116+Kartikey-Tiwari@users.noreply.github.com>
2024-03-04 20:01:27 +0530

    Dropdown menu centered + width issues fixed

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ document.querySelectorAll(".dropdown-container").forEach((el) => {
   const menu = el.querySelector(".dropdown-menu");
   el.addEventListener("mouseenter", () => {
     menu.classList.add("visible");
+    const width = menu.scrollWidth;
+    const left = -(width / 2 - el.offsetWidth / 2);
+    menu.style.left = `${left}px`;
   });
   el.addEventListener("mouseleave", () => {
     menu.classList.remove("visible");
