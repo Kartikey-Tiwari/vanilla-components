5f077fb74828d05a3a1f7a86586a2bf9510bd542
kartikey <98679116+Kartikey-Tiwari@users.noreply.github.com>
2024-03-05 16:27:21 +0530

    Fix menu going out of viewport

--- script.js
+++ script.js
@@ -2,9 +2,23 @@ document.querySelectorAll(".dropdown-container").forEach((el) => {
   const menu = el.querySelector(".dropdown-menu");
   el.addEventListener("mouseenter", () => {
     menu.classList.add("visible");
-    const width = menu.scrollWidth;
-    const left = -(width / 2 - el.offsetWidth / 2);
-    menu.style.left = `${left}px`;
+    const viewportWidth = window.visualViewport.width;
+    const menuWidth = menu.offsetWidth;
+    const { width: toggleWidth, x: toggleX } = el.getBoundingClientRect();
+    let left;
+
+    if (toggleWidth / 2 + toggleX + menuWidth / 2 > viewportWidth) {
+      left =
+        (menuWidth - el.offsetWidth) / 2 +
+        toggleX +
+        (toggleWidth + menuWidth) / 2 -
+        viewportWidth;
+    } else if (toggleX - (menuWidth - toggleWidth) / 2 < 0) {
+      left = toggleX;
+    } else {
+      left = (menuWidth - toggleWidth) / 2;
+    }
+    menu.style.left = `${-left}px`;
   });
   el.addEventListener("mouseleave", () => {
     menu.classList.remove("visible");
