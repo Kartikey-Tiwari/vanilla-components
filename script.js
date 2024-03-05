e5607d75ceac5a1988579db3adc0abf09b7577b0
kartikey <98679116+Kartikey-Tiwari@users.noreply.github.com>
2024-03-05 16:58:13 +0530

    Menu appears either above/below toggle

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 document.querySelectorAll(".dropdown-container").forEach((el) => {
   const menu = el.querySelector(".dropdown-menu");
+  const toggle = el.querySelector(".dropdown-toggle");
+
   el.addEventListener("mouseenter", () => {
     menu.classList.add("visible");
+
     const viewportWidth = window.visualViewport.width;
     const menuWidth = menu.offsetWidth;
-    const { width: toggleWidth, x: toggleX } = el.getBoundingClientRect();
+    const menuHeight = menu.offsetHeight;
+
+    const {
+      width: toggleWidth,
+      x: toggleX,
+      y: toggleY,
+    } = el.getBoundingClientRect();
+
     let left;
+
+    if (toggleY > menuHeight) {
+      menu.style.top = `${-menuHeight}px`;
+      menu.style.maxHeight = `${toggleY}px`;
+      toggle.classList.add("upward");
+    } else {
+      menu.style.maxHeight = "";
+      toggle.classList.remove("upward");
+    }
 
     if (toggleWidth / 2 + toggleX + menuWidth / 2 > viewportWidth) {
       left =
